e5b120160c9b11fb50f4cd0f3b60bde12a718fc2
ss-Akuma <amindashti2019@gmail.com>
2024-10-20 14:49:43 +0330

    signup first implement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
     use std::io::{self, Write};
     use std::collections::HashMap;
-    fn sign_up(){
-        println!("you've entered signup")
+    fn sign_up(user_info: &mut HashMap<String,String>){
+        println!("you've entered signup");
+        loop{
+            println!("please enter your username: ");
+            let mut user_name = String::new();
+            io::stdin().read_line(&mut user_name).expect("error");
+            let user_name = user_name.trim().to_string();
+
+            if user_info.contains_key(&user_name) {
+                println!("username exists!");
+            }else {
+                println!("please enter your password: ");
+                let mut input_password = String::new(); 
+                io::stdin().read_line(&mut input_password).expect("error");
+                let input_password = input_password.trim().to_string();
+
+                user_info.insert(user_name, input_password);
+                println!("you've signed up succesfully!");
+                return;
+            }
+        }
+
+
+        
+            
+
     }
 
 
@@ -14,16 +38,18 @@
         
         match user_info.get(user_name){
             Some(password) => {
-                println!("please enter your password: ");
-                let mut input_password = String::new(); 
-                io::stdin().read_line(&mut input_password).expect("error");
-                let input_password = input_password.trim();
-                if password == input_password{
-                    println!("you've logged in")
-                }else{
-                    println!("password incorrect!")
+                loop {
+                    println!("please enter your password: ");
+                    let mut input_password = String::new(); 
+                    io::stdin().read_line(&mut input_password).expect("error");
+                    let input_password = input_password.trim();
+                    if password == input_password{
+                        println!("you've logged in");
+                        break;
+                    }else{
+                        println!("password incorrect!")
+                    }
                 }
-                
             },
             None => println!("username not found!") 
         }
@@ -40,21 +66,17 @@
         
         loop{
             let mut operation = String::new();
-
             io::stdin().read_line(&mut operation).expect("error");
-
             io::stdout().flush().unwrap();
-
             let opr = operation.trim().to_lowercase();
 
             match opr.as_str() {
                 "login" => {
-                    
                     log_in(&user_info);
                     break;
                 }
                 "signup" => {
-                    sign_up();
+                    sign_up(&mut user_info);
                     break;
                 }
                 "exit" => {
