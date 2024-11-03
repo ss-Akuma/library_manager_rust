21647c977fb254b3da8842ac36b363961ec7cef9
ss-Akuma <amindashti2019@gmail.com>
2024-11-03 19:57:57 +0330

    books init

---
+++ src/books.rs
@@ -0,0 +1,45 @@
+mod id_generator;
+pub struct Book{
+    name:String,
+    year:u32,
+    author:String,
+    id:String
+}
+
+impl Book {
+    pub fn new(&self,name:String,year:u32,author:String) -> Self{
+        Self{
+            name:name,
+            year:year,
+            author:author,
+            id:id_generator::generate_custom_id(10)
+        }
+    }
+    pub fn get_name(&self) -> &str{
+        &self.name
+    }
+    pub fn get_year(&self) -> &u32{
+        &self.year                                                                                    
+    }
+    pub fn get_author(&self) -> &str{
+        &self.author    
+    }
+    pub fn get_id(&self) -> &str{
+        &self.id    
+    }
+    pub fn set_name(&mut self,new_name:String){
+        self.name = new_name;
+    }
+    pub fn set_year(&mut self,new_year:u32){
+        self.year = new_year;
+    }
+    pub fn set_author(&mut self,new_author:String){
+        self.author = new_author;
+    }
+    // pub fn set_id(&self){
+
+    // }
+    
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -43,58 +43,32 @@ edit_user\nlogout\nexit :");
 }
 
 
-fn search_book(){
-
-}
-
-fn edit_book(){
-
-}
-
-fn borrow_book(){
-
-}
-
-fn add_book(){
-
-}
-
-fn delete_book(){
-
-}
-
-fn edit_user(){
-
-}
-
-fn logout(){
-
-}
-
 //fn 
 fn main() {
     let mut  user_db: HashMap<String, User> = HashMap::new();
     let mut _is_logged_in = false;
 
     while !_is_logged_in{
-        println!("please choose your operation (login/signup/exit): ");
         let mut operation = String::new();
+
+        println!("please choose your operation (login/signup/exit): ");
         io::stdin().read_line(&mut operation).expect("error");
         io::stdout().flush().unwrap();
+
         let opr = operation.trim().to_lowercase();
 
         match opr.as_str() {
             "login" => {
+                let mut user_name = String::new();
+                let mut input_password = String::new();
 
                 println!("### login ###");
 
                 println!("please enter your username: ");
-                let mut user_name = String::new();
                 io::stdin().read_line(&mut user_name).expect("error");
                 let user_name = user_name.trim().to_string();
         
                 println!("please enter your password: ");
-                let mut input_password = String::new(); 
                 io::stdin().read_line(&mut input_password).expect("error");
                 let input_password = input_password.trim().to_string();
 
@@ -112,17 +86,21 @@ fn main() {
                 println!("### signup ###");
 
                 loop{
+                    let mut user_name: String = String::new();
+                    let mut input_password: String = String::new(); 
+
                     println!("please enter your username: ");
-                    let mut user_name = String::new();
+                    io::stdout().flush().unwrap();
                     io::stdin().read_line(&mut user_name).expect("error");
                     let user_name = user_name.trim().to_string();
             
                     println!("please enter your password: ");
-                    let mut input_password = String::new(); 
+                    io::stdout().flush().unwrap();
                     io::stdin().read_line(&mut input_password).expect("error");
                     let input_password = input_password.trim().to_string();
             
                     let signup_succes = User::signup(user_name,input_password,&mut user_db);
+
                     if signup_succes {
                         println!("signup succesful!");
                         break;
